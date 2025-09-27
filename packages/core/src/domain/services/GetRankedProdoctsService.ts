f1a006205ae79eeb06aa6f3955c685511831146f
xion6 <u.xion6@gmail.com>
2025-09-27 21:49:19 +0900

    feat: add lint:fix script and update turbo tasks for improved linting and formatting

--- packages/core/src/domain/services/GetRankedProdoctsService.ts
+++ packages/core/src/domain/services/GetRankedProdoctsService.ts
@@ -1,17 +1,17 @@
-import "reflect-metadata";
-import { injectable, inject } from "tsyringe";
-import { GetRankedProdoctsUseCase } from "../../port/in/GetRankedProductsUseCase";
-import { GetProductsPort } from "../../port/out/GetProductsPort";
-import { ProductsEntity } from "../entities/Product";
+import 'reflect-metadata'
+import { injectable, inject } from 'tsyringe'
+import { GetRankedProdoctsUseCase } from '../../port/in/GetRankedProductsUseCase'
+import { GetProductsPort } from '../../port/out/GetProductsPort'
+import { ProductsEntity } from '../entities/Product'
 
 @injectable()
 export class GetRankedProdoctsService implements GetRankedProdoctsUseCase {
   constructor(
-    @inject("GetProductsPort") private readonly productsPort: GetProductsPort,
+    @inject('GetProductsPort') private readonly productsPort: GetProductsPort
   ) {}
 
   async execute(): Promise<ProductsEntity> {
-    const products = await this.productsPort.getProducts();
-    return new ProductsEntity(products).sortByRank();
+    const products = await this.productsPort.getProducts()
+    return new ProductsEntity(products).sortByRank()
   }
 }
