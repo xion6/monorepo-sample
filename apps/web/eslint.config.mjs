a240f1e765044042d41833589ba2884e021abc64
xion6 <u.xion6@gmail.com>
2025-09-28 13:48:48 +0900

    refactor: consolidate ESLint configurations and remove obsolete files

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,5 +1,16 @@
-import nextConfig from '@ecommerce/eslint-config/next'
+const { FlatCompat } = require('@eslint/eslintrc')
+const { fixupConfigRules } = require('@eslint/compat')
+const prettier = require('eslint-config-prettier')
 
-const eslintConfig = [...nextConfig]
+const flatCompat = new FlatCompat()
 
-export default eslintConfig
+/**
+ * @type {import("eslint").Linter.Config}
+ */
+module.exports = [
+  ...fixupConfigRules(
+    flatCompat.extends('next/core-web-vitals'),
+    flatCompat.extends('next/typescript')
+  ),
+  prettier,
+]
--- packages/core/eslint.config.mjs
+++
@@ -1,4 +0,0 @@
-import cleanArchitectureConfig from '@ecommerce/eslint-config/clean-architecture'
-import nodeConfig from '@ecommerce/eslint-config/node'
-
-export default [...nodeConfig, ...cleanArchitectureConfig]
